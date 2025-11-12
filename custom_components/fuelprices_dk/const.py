00455ffb52a731560b7636904fb5fffc17bcb3c0
Jan Larsen <jan@20205255.dk>
2025-11-12 11:39:02 +0100

    Update credits and domain in const.py

--- custom_components/fuelprices_dk/const.py
+++ custom_components/fuelprices_dk/const.py
@@ -4,9 +4,9 @@ CONF_FUELTYPES = "fueltypes"
 CONF_UPDATE_INTERVAL = "update_interval"
 CONF_PLATFORM = "sensor"
 CREDITS = [
-    {"Created by": "J-Lindvig (https://github.com/J-Lindvig)"},
-    {"Techinal support": "Peer Jensen (www.fuelfinder.dk)"},
+    {"Created by": "J-Lindvig (https://github.com/stofakiller)"},
+    {"Techinal support": "Jan Larsen (jan(at)20205255.dk)"},
 ]
-DOMAIN = "fuelprices_dk"
+DOMAIN = "20205255_dk"
 PATH = "./custom_components/" + DOMAIN + "/"
 UPDATE_INTERVAL = 60
